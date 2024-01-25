f47b01233168036491ab3e091313b98ce2e7dcea
narendrakorrapati <korrapatinarendra843@gmail.com>
2024-01-25 09:52:58 +0530

    API Gateway related changes

--- src/main/java/com/narendra/controller/HomeController.java
+++ src/main/java/com/narendra/controller/HomeController.java
@@ -5,12 +5,14 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.narendra.config.DataSourceConfig;
 
 @RestController
 @RefreshScope
+@RequestMapping("/api/v1/home")
 public class HomeController {
 
 	@Value("${spring.jpa.show-sql}")
